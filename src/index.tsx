8fae67a960ed481a6aee4bcae77eb62fbe1faa19
Mahadevan Reji <rmahadevan574@gmail.com>
2025-11-03 00:20:03 +0530

    Update dependencies and refactor index.tsx for database integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
-import { Hono } from 'hono'
-import { renderer } from './renderer'
-import App from './App'
-import Blog from './pages/Blog'
-import { testPost } from './data'
+import { Hono } from "hono";
+import { renderer } from "./renderer";
+import Blog from "./pages/Blog";
+import type { Post } from "./data";
+import { console, D1Database } from "@cloudflare/workers-types";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    db: D1Database;
+  };
+}>();
 
-app.use(renderer)
+app.use(renderer);
 
-app.get('/', (c) => {
-  return c.render(<Blog post={testPost} />)
-})
+app.get("/:slug", async (c) => {
 
-export default app
+  const db = c.env.db;
+  const { slug } = c.req.param();
+  
+  // Fetch the blog post from the database
+  const post = await db.prepare("SELECT * FROM posts WHERE slug = ?")
+    .bind(slug)
+    .first<Post>();
+  if (!post) {
+    return c.text("Post not found", 404);
+  }
+  
+  return c.render(<Blog post={post} />);
+});
+
+export default app;
