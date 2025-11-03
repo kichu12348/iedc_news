ad7ed70a346e9e8f8943fb0a6aea745675402222
Mahadevan Reji <rmahadevan574@gmail.com>
2025-11-03 22:49:30 +0530

    Update footer layout: center content and add spacing for improved aesthetics

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -89,18 +89,18 @@ export const BlogPostPage = ({ post }: BlogPostProps) => {
             <img src="/iedc_logo.svg" alt="IEDC Logo" />
           </div>
           <p>© 2025 IEDC Bootcamp CEC</p>
-          <div className="not-found-credit">
-            <span>Made Wid </span>
-            <svg
-              className="heart-icon"
-              viewBox="0 0 24 24"
-              fill="#ff0000"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-            </svg>
-            <span> by Kichu</span>
-          </div>
+        </div>
+        <div className="not-found-credit">
+          <span>Made Wid </span>
+          <svg
+            className="heart-icon"
+            viewBox="0 0 24 24"
+            fill="#ff0000"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+          </svg>
+          <span> by Kichu</span>
         </div>
       </footer>
     </div>
